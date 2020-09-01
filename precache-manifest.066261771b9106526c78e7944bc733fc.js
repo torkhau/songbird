bb6fac28843760d1844ba67a51d37149aa4e2b44
Torkhau <torkhau@gmail.com>
2020-09-02 00:13:16 +0300

    Updates

---
+++ precache-manifest.066261771b9106526c78e7944bc733fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0fb8e8d1c458c8830c334622900edd9",
+    "url": "/songbird/index.html"
+  },
+  {
+    "revision": "411be65b6d08e8e7a004",
+    "url": "/songbird/static/css/main.8960a983.chunk.css"
+  },
+  {
+    "revision": "5d03d2dfa6bb89e23777",
+    "url": "/songbird/static/js/2.490980b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/songbird/static/js/2.490980b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411be65b6d08e8e7a004",
+    "url": "/songbird/static/js/main.a413b9db.chunk.js"
+  },
+  {
+    "revision": "41fc37381d3a97757444",
+    "url": "/songbird/static/js/runtime-main.710c5ffc.js"
+  },
+  {
+    "revision": "06a469380cb09a64d372f773665e7500",
+    "url": "/songbird/static/media/emptybird.06a46938.jpg"
+  },
+  {
+    "revision": "0d551de359b309b86cd80a772cdd9436",
+    "url": "/songbird/static/media/fall.0d551de3.mp3"
+  },
+  {
+    "revision": "25c941b30a8e6cc943e88e954a5a8bcf",
+    "url": "/songbird/static/media/icomoon.25c941b3.svg"
+  },
+  {
+    "revision": "5a7f651ea6b8ce3035958c96e6607eee",
+    "url": "/songbird/static/media/icomoon.5a7f651e.ttf"
+  },
+  {
+    "revision": "6e9aae82e1ded31c103c805bd7214785",
+    "url": "/songbird/static/media/icomoon.6e9aae82.woff"
+  },
+  {
+    "revision": "fd3df2d952e0d0f2f44cad470e9bcd43",
+    "url": "/songbird/static/media/icomoon.fd3df2d9.eot"
+  },
+  {
+    "revision": "4ea89a5fef34201a32ed6f9cb6f15692",
+    "url": "/songbird/static/media/win.4ea89a5f.mp3"
+  }
+]);
